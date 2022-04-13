6d6be21b670b26c1b07cb80a940eb298e514b020
Jozef Karabelly <jozefkarabelly@gmail.com>
2022-04-13 17:10:37 +0200

    fix register template path

--- file/views.py
+++ file/views.py
@@ -16,7 +16,7 @@ def signup(request):
             return redirect('home')
     else:
         form = UserCreationForm()
-    return render(request, 'signup.html', {'form': form})
+    return render(request, 'file/register.html', {'form': form})
 
 
 class HomeView(TemplateView):
