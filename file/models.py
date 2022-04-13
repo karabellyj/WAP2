3c91c233036eeccb9fba57a56c03e4f5d2e00f2c
Jozef Karabelly <jozefkarabelly@gmail.com>
2022-04-13 16:48:29 +0200

    add file model

--- file/models.py
+++ file/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from hashlib import md5
 
-# Create your models here.
+
+class File(models.Model):
+    url_hash = models.URLField(unique=True)
+    file = models.BinaryField()
+    expire_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    visits = models.PositiveIntegerField(default=0)
+
+    def clicked(self):
+        visits += 1
+        self.save()
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.url_hash = md5(self.file.encode()).hexdigest()[:10]
+        
+        return super().save(*args, **kwargs)
